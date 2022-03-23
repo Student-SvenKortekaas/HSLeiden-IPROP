dd7ce3236985843bb5ae16ce26abb0bc491bef6a
Sven Kortekaas <s1132619@student.hsleiden.nl>
2022-03-23 14:23:15 +0100

    Update logic.py

--- app/logic.py
+++ app/logic.py
@@ -1,3 +1,19 @@
+from typing import List
+
 from app.database import get_data
 
 
+def filter_played_games(cursor, choices: List[int]) -> List[int]:
+    """
+    Limit the list of games to only the games the user has played.
+    """
+
+    played_games = []
+
+    games = get_data(cursor, table="game")
+
+    for game in games:
+        if game[0] in choices:
+            played_games.append(game[0])
+
+    return played_games
